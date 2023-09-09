dab63ca18be913694f788756ec008ab1c34b85aa
StoneyDSP <nathanjhood@googlemail.com>
2023-09-09 18:56:10 +0200

    Added 'stoneydsp_types'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/StoneyDSP/modules/stoneydsp_core/types/stoneydsp_types.h
@@ -0,0 +1,94 @@
+/***************************************************************************//**
+ * @file stoneydsp_types.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+#pragma once
+
+#define __STONEYDSP_TYPES_H__
+
+namespace StoneyDSP
+{
+namespace Core
+{
+/**
+ * @brief The 'StoneyDSP::Core::Types' namespace.
+ *
+ */
+namespace Types
+{
+
+} // namespace Types
+} // namespace Core
+} // namespace StoneyDSP
+
+#define Void_t void
+
+#define Char_t char
+#define WChar_t wchar_t
+#define Bool_t bool
+#define Int_t int
+#define Float_t float
+#define Double_t double
+#define Short_t short
+#define Long_t long
+// #define Signed_t signed
+// #define Unsigned_t unsigned
+
+typedef Void_t Void;
+
+typedef Char_t Char;
+typedef WChar_t WChar;
+typedef Bool_t Bool;
+typedef Int_t Int;
+typedef Float_t Float;
+typedef Double_t Double;
+typedef Short_t Short;
+typedef Long_t Long;
+
+#define Class_t class
+#define Struct_t struct
+
+// typedef Class_t Class;
+// typedef Struct_t Struct;
+
+#define Interface Class_t
+
+#define Public public:
+#define Private private:
+#define Protected protected:
+
+#define Const const
+#define NoExcept noexcept
+
+// #define ptr *
+// #define ref &
+
+// #define ADD +
+// #define SUB -
+// #define MUL *
+// #define DIV /
+
+// #define add ADD
+// #define sub SUB
+// #define mul MUL
+// #define div DIV
+
+// #define EQ ==
+// #define NE !=
+// #define GT >
+// #define GE >=
+// #define LT <
+// #define LE <=
+
+// #define eq EQ
+// #define ne NE
+// #define gt GT
+// #define ge GE
+// #define lt LT
+// #define le LE
