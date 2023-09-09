d4b4e6bb6dc91372d84006e43be9585ec8c24f08
StoneyDSP <nathanjhood@googlemail.com>
2023-09-09 19:01:47 +0200

    Added 'main'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/main.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************//**
+ * @file main.cpp
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+
+#include "StoneyDSP/stoneydsp.cpp"
+
+#include "main.h"
+
+Int Main(Int argc, Char* argv[])
+{
+
+    for(Int i = 0; i < argc; ++i)
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+
+    std::cout << std::endl;
+
+
+    using namespace StoneyDSP::Core::Types;
+
+    {
+        using namespace StoneyDSP::Core::Memory;
+
+        unique_ptr<Value<Float>> numPtr = make_unique<Value<Float>>();
+
+        std::cin.get();
+    }
+
+    std::cin.get();
+
+    return 0;
+}
---
+++ include/main.h
@@ -0,0 +1,112 @@
+/***************************************************************************//**
+ * @file main.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+#pragma once
+
+#define __MAIN_H__
+
+#include "StoneyDSP/stoneydsp.h"
+
+/**
+ * @brief Functions within the source program perform one or more specific
+ * tasks. The 'wmain' function can call these functions to perform their
+ * respective tasks. When 'wmain' calls another function, it passes execution
+ * control to the function, so that execution begins at the first statement in
+ * the function. A function returns control to 'wmain' when a return statement is
+ * executed or when the end of the function is reached.
+ *
+ * @return int
+ */
+Int wmain(void);
+
+/**
+ * @brief Functions within the source program perform one or more specific
+ * tasks. The 'wmain' function can call these functions to perform their
+ * respective tasks. When 'wmain' calls another function, it passes execution
+ * control to the function, so that execution begins at the first statement in
+ * the function. A function returns control to 'wmain' when a return statement is
+ * executed or when the end of the function is reached.
+ *
+ * @param argc Number of strings in array 'argv'.
+ * @param argv Array of command-line argument strings.
+ * @return int
+ */
+Int wmain(Int argc, wchar_t* argv[]);
+
+/**
+ * @brief Functions within the source program perform one or more specific
+ * tasks. The 'wmain' function can call these functions to perform their
+ * respective tasks. When 'wmain' calls another function, it passes execution
+ * control to the function, so that execution begins at the first statement in
+ * the function. A function returns control to 'wmain' when a return statement is
+ * executed or when the end of the function is reached.
+ *
+ * @param argc Number of strings in array 'argv'.
+ * @param argv Array of command-line argument strings.
+ * @param envp Array of environment variable strings.
+ * @return int
+ */
+Int wmain(Int argc, wchar_t* argv[], wchar_t* envp[]);
+
+// /**
+//  * @brief Functions within the source program perform one or more specific
+//  * tasks. The 'main' function can call these functions to perform their
+//  * respective tasks. When 'main' calls another function, it passes execution
+//  * control to the function, so that execution begins at the first statement in
+//  * the function. A function returns control to 'main' when a return statement is
+//  * executed or when the end of the function is reached.
+//  *
+//  * @return int
+//  */
+// Int main(void);
+
+/**
+ * @brief Functions within the source program perform one or more specific
+ * tasks. The 'main' function can call these functions to perform their
+ * respective tasks. When 'main' calls another function, it passes execution
+ * control to the function, so that execution begins at the first statement in
+ * the function. A function returns control to 'main' when a return statement is
+ * executed or when the end of the function is reached.
+ *
+ * @param argc Number of strings in array 'argv'.
+ * @param argv Array of command-line argument strings.
+ * @return int
+ */
+Int main(Int argc, Char* argv[]);
+
+// /**
+//  * @brief Functions within the source program perform one or more specific
+//  * tasks. The 'main' function can call these functions to perform their
+//  * respective tasks. When 'main' calls another function, it passes execution
+//  * control to the function, so that execution begins at the first statement in
+//  * the function. A function returns control to 'main' when a return statement is
+//  * executed or when the end of the function is reached.
+//  *
+//  * @param argc Number of strings in array 'argv'.
+//  * @param argv Array of command-line argument strings.
+//  * @param envp Array of environment variable strings.
+//  * @return int
+//  */
+// Int main(Int argc, Char* argv[], Char* envp[]);
+
+#ifdef UNICODE
+ #define Main wmain
+#else
+ #define Main main
+#endif
+
+/**
+ * @brief The global namespace.
+ *
+ */
+namespace
+{
+
+} // namespace
