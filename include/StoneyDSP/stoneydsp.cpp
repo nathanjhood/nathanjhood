14dbfb528f21c7a37286de767de834b21eb3bdac
StoneyDSP <nathanjhood@googlemail.com>
2023-09-09 19:01:26 +0200

    Added 'stoneydsp'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/StoneyDSP/stoneydsp.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************//**
+ * @file stoneydsp.cpp
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+
+/**
+ * @brief The 'StoneyDSP' namespace.
+ *
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @version 1.0.0-init
+ * @date 2023-09-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+namespace StoneyDSP
+{
+
+} // namespace StoneyDSP
+
+#include "stoneydsp.h"
+
+#include "modules/stoneydsp_core/stoneydsp_core.cpp"
---
+++ include/StoneyDSP/stoneydsp.h
@@ -0,0 +1,15 @@
+/***************************************************************************//**
+ * @file stoneydsp.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+#pragma once
+
+#define __STONEYDSP_H__
+
+#include "modules/stoneydsp_core/stoneydsp_core.h"
